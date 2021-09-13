97d964bebf2c872698db1a3c81674fb358e65171
Axel <axeljcm@estudiantec.cr>
2021-09-13 19:02:33 -0400

    Mouse movement y keypressed

--- Server/code/main.cpp
+++ Server/code/main.cpp
@@ -1,16 +1,25 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Test");
-    while(window.isOpen())
-    {
-        sf::Event event{};
-        while(window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-                window.close();
+int main() {
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Breakout");
+    while (window.isOpen()) {
+        sf::Event event{};{
         }
-    }
-    return 0;
-}
+        while (window.pollEvent(event)) {
+            //Ejemplo de mousemovement dentro de la pantalla
+            if (event.type == sf::Event::MouseMoved)
+                std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
+            std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
+
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    std::cout << "the escape key was pressed" << std::endl;
+                }}
+            if (event.type == sf::Event::Closed)
+                        window.close();
+
+                }
+            }
+            return 0;
+        }
